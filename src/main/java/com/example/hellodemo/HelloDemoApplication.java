0363af684f32494f32acf4097ac4b04239dcca8f
htadepal <Hema03@ge>
2022-05-15 18:39:18 +0200

    INitial commit.

--- src/main/java/com/example/hellodemo/HelloDemoApplication.java
+++ src/main/java/com/example/hellodemo/HelloDemoApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class HelloDemoApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello>>>>");
         SpringApplication.run(HelloDemoApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public Message displayMessage() {
+        return "Greetings!";
     }
 
 }
